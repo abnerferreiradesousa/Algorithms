aa39a5455c8449a4c707f1314cf661ea1fbf824d
Abner <abnerferreira84@gmail.com>
2022-10-10 15:59:45 -0300

    feat: requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,12 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
+    try:
+        count = 0
+        for item in permanence_period:
+            if item[0] <= target_time <= item[1]:
+                count += 1
+    except TypeError:
+        return None
+
+    return count
+    
