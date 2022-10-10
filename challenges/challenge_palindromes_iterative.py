0e328b4c51ca6a82a1add0d33696f5f039893b67
Abner <abnerferreira84@gmail.com>
2022-10-10 18:36:50 -0300

    feat: requisito 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,6 @@
 def is_palindrome_iterative(word):
     """Faça o código aqui."""
+    if word == "":
+        return False
+    reversed_word = "".join(reversed(word))
+    return word == reversed_word
