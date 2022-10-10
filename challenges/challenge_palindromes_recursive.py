53833798dc119dafc70b3a35aa20f7103571b135
Abner <abnerferreira84@gmail.com>
2022-10-10 18:28:19 -0300

    feat: requisito 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,19 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
+    # print(f"Word -> {word} --- LI -> {low_index} --- HI -> {high_index}")
+    # Word -> REVIVER --- LI -> 0 --- HI -> 199
+
+    if word == "": 
+        return False
+
+    if low_index < len(word):
+        other_word = word[low_index] == word[high_index]
+
+    if low_index >= high_index:
+        return True
+
+    if other_word == False:
+        return False
+
+    if other_word == True:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
