8d345a035098a54fe967be928216faa4416409d1
Abner <abnerferreira84@gmail.com>
2022-10-11 17:58:05 -0300

    fix: melhorando performance requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,19 +1,31 @@
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
-    list_counter_letters = []
 
     # Case One
     # pedrra ==== pedraa
     # list_counter_letters = ["p", "e", "d", "r", "a"]
 
-    for letter in first_string.lower():
-        if (
-            letter in second_string.lower()
-            and letter not in list_counter_letters
-        ):
-            list_counter_letters.append(letter)
-            first_string = "".join(list_counter_letters)
+    if len(first_string) != len(second_string):
+        return False
+    first_string_list = list(first_string.lower())
+    second_string_list = list(second_string.lower())
+    for letter in first_string_list:
+        try:
+            second_string_list.remove(letter)
+        except ValueError:
+            return False
+    return True
 
-    if len(first_string) == len(second_string):
-        return True
-    return False
+    # Version one...
+    # list_counter_letters = []
+    # for letter in first_string.lower():
+    #     if (
+    #         letter in second_string.lower()
+    #         and letter not in list_counter_letters
+    #     ):
+    #         list_counter_letters.append(letter)
+    #         first_string = "".join(list_counter_letters)
+
+    # if len(first_string) == len(second_string):
+    #     return True
+    # return False
--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,24 @@
-def find_duplicate(nums):
+from timeit import repeat
+from typing import List
+
+
+def find_duplicate(nums: List[int]) -> int:
     """Faça o código aqui."""
+
+    # Case one
+        # nums = [3, 1, 3, 4, 2]
+        # saída: 3
+
+    if len(nums) < 1:
+        return False
+
+    repeat_nums = []
+    nums.sort()
+    for num in nums:
+        if num > 0:
+            if num in repeat_nums:
+                return num
+            else:
+                repeat_nums.append(num)
+        else:
+            return False
