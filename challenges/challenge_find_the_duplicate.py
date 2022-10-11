1019c5091398ca3f7185dfe7a906ce31e43ae7d2
Abner <abnerferreira84@gmail.com>
2022-10-11 19:32:10 -0300

    feat: requisito 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -3,21 +3,44 @@ from typing import List
 
 def find_duplicate(nums: List[int]) -> int:
     """Faça o código aqui."""
-
-    # Case one
-    # nums = [3, 1, 3, 4, 2]
-    # saída: 3
-
-    if len(nums) < 1:
+    length = len(nums)
+    if length == 1 or length == 0 or nums is None:
         return False
 
-    repeat_nums = []
+    index = 0
     nums.sort()
-    for num in nums:
-        if num > 0:
-            if num in repeat_nums:
-                return num
-            else:
-                repeat_nums.append(num)
-        else:
-            return False
+
+    [1, 3, 4, 5, 1]
+    try:
+        for num in nums:
+            index += 1
+            if isinstance(num, str) or num < 0:
+                return False
+            if num == nums[index]:
+                    return num
+    except IndexError:
+        return False
+
+    # Version one
+    # if len(nums) == 1 or nums is None:
+    #     return False
+
+    # index = 1
+    # nums.sort()
+
+    # [1, 3, 4, 5, 1]
+
+    # for num in nums:
+
+    #     if isinstance(num, str) or num < 0:
+    #         return False
+
+    #     while (index < len(nums)):
+    #         print(nums)
+    #         print(f"{num} é igual {nums[index]}")
+
+    #         if num == nums[index]:
+    #             return num
+    #         index += 1
+    #     index = 1
+    # return False
