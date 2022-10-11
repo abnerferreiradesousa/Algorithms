f2f3ff8e2b8e70bb0ec2af9719935accfd09c8ca
Abner <abnerferreira84@gmail.com>
2022-10-11 11:02:50 -0300

    fix: consertando lint, error E712 comparison to False should be 'if cond is False:' or 'if not cond:'

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,19 @@
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
+    list_counter_letters = []
+
+    # Case One
+    # pedrra ==== pedraa
+    # list_counter_letters = ["p", "e", "d", "r", "a"]
+
+    for letter in first_string.lower():
+        if (
+            letter in second_string.lower()
+            and letter not in list_counter_letters
+        ):
+            list_counter_letters.append(letter)
+            first_string = "".join(list_counter_letters)
+
+    if len(first_string) == len(second_string):
+        return True
+    return False
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,16 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    # print(f"Word -> {word} --- LI -> {low_index} --- HI -> {high_index}")
-    # Word -> REVIVER --- LI -> 0 --- HI -> 199
-
     if low_index < len(word):
         other_word = word[low_index] == word[high_index]
 
-    if word == "" or other_word == False:
+    if word == "" or other_word is False:
         return False
 
     if low_index >= high_index:
         return True
 
-    if other_word == True:
+    if other_word:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
